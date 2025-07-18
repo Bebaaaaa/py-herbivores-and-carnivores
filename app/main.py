503a208df7c102090096e4d221a8e6272efd7ae8
Argenti <ismartsdn2420@gmail.com>
2025-07-18 08:41:02 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,21 +1,19 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str, health: int = 100) -> None:
-        self.name: str = name
-        self.health: int = health
-        self.hidden: bool = False
-        Animal.alive.append(self)
+    def __init__(self, name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        if self.health > 0:
+            Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return (
-            f"{{Name: {self.name}, Health: {self.health}, "
-            f"Hidden: {self.hidden}}}"
-        )
-
-    @classmethod
-    def __str__(cls) -> str:
-        return str([repr(animal) for animal in cls.alive])
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
 
 
 class Herbivore(Animal):
@@ -24,14 +22,9 @@ class Herbivore(Animal):
 
 
 class Carnivore(Animal):
-    def bite(self, target: str) -> None:
-        if not isinstance(target, Herbivore):
-            return
-        if target.hidden:
-            return
-
-        target.health -= 50
-        if target.health <= 0:
-            target.health = 0
-            if target in Animal.alive:
-                Animal.alive.remove(target)
+    def bite(self, herbivore: Herbivore) -> None:
+        if isinstance(herbivore, Herbivore):
+            if not herbivore.hidden:
+                herbivore.health -= 50
+            if herbivore.health <= 0:
+                Animal.alive.remove(herbivore)
